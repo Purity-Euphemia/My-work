adc505a6e4ae1cc01be6ee77a6f71f1a09de0f3e
cobachie <euphemiaobachie@gmail.com>
2026-05-18 12:28:57 +0100

    first commit

--- stringFormatting/stringFormatting.go
+++ stringFormatting/stringFormatting.go
@@ -7,6 +7,13 @@ type Person struct {
 	Age int
 }
 func main() {
-	fmt.Printf("interger: %d/n", 100)
-	fmt.Printf("Float: %.2f/n", 123.456)
+	fmt.Printf("interger: %d\n", 100)
+	fmt.Printf("Float: %.2f\n", 123.456)
+	// format float with 2 decimal places
+	fmt.Printf("String: %s\n", "Go is fun")
+	fmt.Printf("Boolean: %t\n", true)
+	// Using %+v to display struct with field names
+	p := Person{"Alice", 30}
+	fmt.Printf("Struct: %+v\n", p)
+	fmt.Printf("Type of variable p: %T\n", p)
 }
