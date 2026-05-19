1b33dbee15778f47a4c29a511163d9cbe9676212
cobachie <euphemiaobachie@gmail.com>
2026-05-19 09:58:04 +0100

    first commit

---
+++ reverseArray/reverseArray.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func reverseArray(arr []int) []int {
+	reverse := []int{}
+	for count := len(arr) - 1; count < 0; count--{
+		reverse = append(reverse, arr[count])
+	}
+	return reverse
+}
+
+func main() {
+	numbers := [] int{10, 20, 30, 40}
+	fmt.Println(reverseArray(numbers))
+}
