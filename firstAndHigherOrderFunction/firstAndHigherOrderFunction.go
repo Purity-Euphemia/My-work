b02286e7284228e4a1f48b7db6ac64d80877b4ca
cobachie <euphemiaobachie@gmail.com>
2026-06-01 14:25:08 +0100

    my work

---
+++ firstAndHigherOrderFunction/firstAndHigherOrderFunction.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var greet = func(name string) string {
+	return "Hello," + name
+}
+
+func processGreeting(f func(string)string, name string) {
+	fmt.Println(f(name))
+}
+func main() {
+	message := greet("Spock")
+	fmt.Println(message)
+}
