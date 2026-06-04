0803b4f950efdaf7c15a4beaad91b5be387a6032
cobachie <euphemiaobachie@gmail.com>
2026-06-04 16:53:28 +0100

    my work

---
+++ readingAndWritingFiles/readingAndWritingFiles.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	data, err := os.ReadFile("example.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(string(data))
+	msg := []byte("\nWriting data to the file")
+	data = append(data, msg...)
+	err = os.WriteFile("new_file.txt", data, 0744)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
